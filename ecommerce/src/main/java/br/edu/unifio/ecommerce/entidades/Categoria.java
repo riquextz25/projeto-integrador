03b98908b2d895ccb0ff9e0b102094f7e636a2bc
riquextz25 <campese.rique@gmail.com>
2026-08-20 01:31:35 +0000

    save2

---
+++ ecommerce/src/main/java/br/edu/unifio/ecommerce/entidades/Categoria.java
@@ -0,0 +1,21 @@
+package br.edu.unifio.ecommerce.entidades;
+
+import lombok.Getter;
+import lombok.Setter;
+
+    @Getter
+    @Setter
+    //@NoArgsConstructor 
+    //@AllArgsConstructor
+    
+    //byte short int long Byte Short Integer Long
+    // float double float Double
+    //char Character
+    //boolean Boolean
+public class Categoria {
+
+    private Short id;
+    private String nome;
+    private String descricao;
+
+}
